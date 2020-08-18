ca3cb2ea099740d9510b8affa9ab668804bae41f
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2020-08-18 11:11:49 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "ibm" {
-    version          = ">= 0.18.0"
-    ibmcloud_api_key = "${var.ibmcloud_api_key}"
+    #version          = ">= 0.18.0"
+    #ibmcloud_api_key = "${var.ibmcloud_api_key}"
     region           = "${var.ibmcloud_region}"
     #zone             = "${var.ibmcloud_zone}"
 }
