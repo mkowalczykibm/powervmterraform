e200f63e2cd27ac0a1dabcac962b02e79cbe62fe
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2020-08-18 11:17:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 ## Template to be used by the IBM Provider for Power Systems
 
-resource "local_file" "vm_private_key" {
-    content_base64    = "${var.vm_private_key_base64}"
-    filename          = "tmp/id_rsa"
-}
+#resource "local_file" "vm_private_key" {
+#    content_base64    = "${var.vm_private_key_base64}"
+#    filename          = "tmp/id_rsa"
+#}
 
 data "ibm_pi_network" "power_networks" {
     count                = "${length(var.networks)}"
@@ -31,19 +31,5 @@ resource "ibm_pi_instance" "pvminstance" {
     pi_replication_scheme = "${var.replication_scheme}"
     pi_replicants         = "${var.replicants}"
     pi_cloud_instance_id  = "${var.power_instance_id}"
-
-    provisioner "remote-exec" {
-        scripts = [
-            "scripts/wait_for_vm.sh",
-            "scripts/bootstrap-aix.sh",
-        ]
-
-        connection {
-            type        = "ssh"
-            host        = "${lookup(ibm_pi_instance.pvminstance.addresses[0], "externalip")}"
-            timeout     = "15m"
-            user        = "root"
-            private_key = "${file("${local_file.vm_private_key.filename}")}"
-        }
     }
 }
