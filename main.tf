88e498d9690a8d9cdb79fad0b7b6ba52e81c015b
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2020-08-18 12:36:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,18 +10,13 @@ data "ibm_pi_network" "power_networks" {
     pi_cloud_instance_id = "${var.power_instance_id}"
 }
 
-data "ibm_pi_image" "power_images" {
-    pi_image_name        = "${var.image_name}"
-    pi_cloud_instance_id = "${var.power_instance_id}"
-}
-
 resource "ibm_pi_instance" "pvminstance" {
     pi_memory             = "${var.memory}"
     pi_processors         = "${var.processors}"
     pi_instance_name      = "${var.vm_name}"
     pi_proc_type          = "${var.proc_type}"
     pi_migratable         = "${var.migratable}"
-    pi_image_id           = "${data.ibm_pi_image.power_images.imageid}"
+    pi_image_id           = "${var.image_id}"
     pi_volume_ids         = []
     pi_network_ids        = ["${data.ibm_pi_network.power_networks.*.networkid}"]
     pi_key_pair_name      = "${var.ssh_key_name}"
