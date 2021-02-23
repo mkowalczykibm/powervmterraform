6e85b5962a2392cfa8b4758424ae2087b92112a1
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2021-02-23 10:55:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "ibmcloud_zone" {
 
 variable "vm_name" {
     description = "Name of the VM"
-    default     = "vm-rbi-terraform"
+    default     = "vm-power-terraform"
 }
 
 #variable "vm_private_key_base64" {
