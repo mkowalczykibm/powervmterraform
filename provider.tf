0fdf08c8ba838e4b31d1053f07c941aa8a1e8f4b
mkowalczykibm <m.kowalczyk@pl.ibm.com>
2021-09-14 14:54:40 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.12.0"
+    }
+  }
+}
+
+# Configure the IBM Provider
+
 provider "ibm" {
     #version          = ">= 0.18.0"
     #ibmcloud_api_key = "${var.ibmcloud_api_key}"
